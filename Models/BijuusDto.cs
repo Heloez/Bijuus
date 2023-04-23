a90ea4e58fcd1ecfbd5454193c9acd2c3cde0b66
Heloez <escolaanaehelo@gmail.com>
2023-04-23 20:40:06 -0300

    finalizando

--- Models/BijuusDto.cs
+++
@@ -1,6 +0,0 @@
-namespace Bijuus.Models;
-
-    public class BijuusDto
-    {
-        public List<caracteristicas> caracteristicas { get; set; }
-    }
--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Bijuus.Models;
-
-public class ErrorViewModel
-{
-    public string RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
